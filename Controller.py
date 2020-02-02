cda059f66a20c1740dd19f981dae0e595993cf39
Harley Bates <Harley.Bates@kpmg.co.uk>
2020-02-02 15:39:40 +0000

    Rough idea of Controller.py

--- Controller.py
+++ Controller.py
@@ -7,22 +7,25 @@ import requests
 I want this code to focus on high cohesion and low coupling between components.
 This will be done through a statement on each function that must NOT include the word 'and'
 """
-# Set values - Constants
-end = datetime.datetime.now()
-start = end - datetime.timedelta(days=365)
-
-# Variables - Want Symbols/Base
 
 
-def obtain_values(start_at, end_at):  # Will need to pass through: startdate, enddate, symbol and base
-    response = requests.get(f'https://api.exchangeratesapi.io/history?start_at={start_at.strftime("%Y-%m-%d")}&end_at={end_at.strftime("%Y-%m-%d")}&symbols=GBP')
-    values = response.json().get("rates", {})
-    return(values)
+class Controller():
+    # Set values - Constants
+    end = datetime.datetime.now()
+    start = end - datetime.timedelta(days=365)
+
+    # Variables - Want Symbols/Base
+
+    def obtain_values(start_at, end_at):  # Will need to pass through: startdate, enddate, symbol and base
+        response = requests.get(f'https://api.exchangeratesapi.io/history?start_at={start_at.strftime("%Y-%m-%d")}&end_at={end_at.strftime("%Y-%m-%d")}&symbols=GBP')
+        values = response.json().get("rates", {})
+        return(values)
+
+    def generate_graph(dates, currencies):
+        return(plot([Scatter(x=dates, y=currencies)], output_type='div'))
 
 
-def generate_graph(dates, currencies):
-    fig = plot([Scatter(x=dates, y=currencies)], output_type='div')
-
+# The below will no longer be needed (but shows how the process will work - some will be moved to View.py)
 if __name__ == '__main__':
     values = obtain_values(start, end)
     dates = [date for date, currencies in sorted(values.items())]
